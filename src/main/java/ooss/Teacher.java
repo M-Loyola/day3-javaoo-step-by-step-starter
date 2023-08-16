e8df81ab590af06dd241047a3d8a6bb9cac9bd7d
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-16 21:22:07 +0800

    refactor: modify for meaningful names

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -5,34 +5,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
-    private List<Klass> classesTeaching = new ArrayList<>();
+    private List<Klass> taughtClasses = new ArrayList<>();
 
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
     public void assignTo(Klass klass) {
-        if (!classesTeaching.contains(klass)) {
-            classesTeaching.add(klass);
+        if (!taughtClasses.contains(klass)) {
+            taughtClasses.add(klass);
         }
     }
 
     public boolean belongsTo(Klass klass) {
-        return classesTeaching.contains(klass);
+        return taughtClasses.contains(klass);
     }
 
     public boolean isTeaching(Student student) {
-        return classesTeaching.stream().anyMatch(klass -> student.isIn(klass));
+        return taughtClasses.stream().anyMatch(klass -> student.isIn(klass));
     }
 
     @Override
     public String introduce() {
-        String classNumbers = classesTeaching.stream().map(klass -> String.valueOf(klass.getNumber())).collect(Collectors.joining(", "));
-        if (classesTeaching.isEmpty()) {
+        String classNumbers = taughtClasses.stream().map(klass -> String.valueOf(klass.getNumber())).collect(Collectors.joining(", "));
+        if (taughtClasses.isEmpty()) {
             return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
         }
-        return (classesTeaching.size() == 1) ?
-                String.format("My name is %s. I am %d years old. I am a teacher. I teach Class %d.", name, age, classesTeaching.get(0).getNumber()) :
+        return (taughtClasses.size() == 1) ?
+                String.format("My name is %s. I am %d years old. I am a teacher. I teach Class %d.", name, age, taughtClasses.get(0).getNumber()) :
                 String.format("My name is %s. I am %d years old. I am a teacher. I teach Class %s.", name, age, classNumbers);
     }
 
