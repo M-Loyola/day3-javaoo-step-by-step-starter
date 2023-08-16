7d6f0716ae8fc8bb673a63a4a90ad14ae2a78347
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-16 18:40:26 +0800

    feat: should_return_result_by_is_belong_to_when_belongsTo_given_teacher_teaches_in_the_class

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,17 +1,42 @@
 package ooss;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
+    private List<Klass> classesTeaching = new ArrayList<>();
+
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
+    public void assignTo(Klass klass) {
+        if (!classesTeaching.contains(klass)) {
+            classesTeaching.add(klass);
+        }
+    }
 
+    public boolean belongsTo(Klass klass) {
+        return classesTeaching.contains(klass);
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : classesTeaching) {
+            if (student.isIn(klass)) {
+                return true;
+            }
+        }
+        return false;
+    }
     @Override
     public String introduce() {
-        return String.format("My name is %s. I am %d years old. I am a teacher.",name,age);
+        if (classesTeaching.size() == 1) {
+            return String.format("My name is %s. I am %d years old. I am a teacher. I teach Class %d.", name, age, classesTeaching.get(0).getNumber());
+        } else if (classesTeaching.size() > 1) {
+            String classNumbers = classesTeaching.stream().map(klass -> String.valueOf(klass.getNumber())).collect(Collectors.joining(", "));
+            return String.format("My name is %s. I am %d years old. I am a teacher. I teach Class %s.", name, age, classNumbers);
+        } else {
+            return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
+        }
     }
 }
