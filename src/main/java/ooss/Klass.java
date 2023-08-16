93495cdfc276f10535f1469b0313506304e6e408
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-16 18:26:54 +0800

    feat: should_return_false_when_equals_given_two_klass_with_different_id

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -2,4 +2,27 @@ package ooss;
 
 public class Klass {
 
+    private final int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Klass klass = (Klass) obj;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,13 +1,27 @@
 package ooss;
 
+import java.util.Objects;
+
 public class Student extends Person {
+
+    private Klass klass;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public void join (Klass klass) {
+        this.klass = klass;
+    }
+    public boolean isIn(Klass klass) {
+        return this.klass != null && this.klass.equals(klass);
+    }
+
     @Override
     public String introduce() {
-        return String.format("My name is %s. I am %d years old. I am a student.",name,age);
+        return isIn(klass) ?
+                String.format("My name is %s. I am %d years old. I am a student. I am in class %d.", name, age, klass.getNumber())
+                :
+                String.format("My name is %s. I am %d years old. I am a student.",name,age);
     }
 }
