d747f0e2ebd9d9ef97e088e87bfcf8fe195c2eb0
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-16 18:48:50 +0800

    feat: should_return_result_when_isLeader_given_student

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -3,6 +3,7 @@ package ooss;
 public class Klass {
 
     private final int number;
+    private Student leader;
 
     public Klass(int number) {
         this.number = number;
@@ -11,7 +12,17 @@ public class Klass {
     public int getNumber() {
         return number;
     }
+    public void assignLeader(Student student) {
+        if (student.isIn(this)) {
+            leader = student;
+        } else {
+            System.out.println("It is not one of us.");
+        }
+    }
 
+    public boolean isLeader(Student student) {
+        return leader != null && leader.equals(student);
+    }
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,27 +1,40 @@
 package ooss;
 
-import java.util.Objects;
-
 public class Student extends Person {
 
     private Klass klass;
+    private boolean isLeader = false;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
-    public void join (Klass klass) {
+    public void join(Klass klass) {
         this.klass = klass;
     }
+
     public boolean isIn(Klass klass) {
         return this.klass != null && this.klass.equals(klass);
     }
 
+    public void assignLeader() {
+        if (klass != null) {
+            klass.assignLeader(this);
+        }
+    }
+
+    public void setLeader(boolean isLeader) {
+        this.isLeader = isLeader;
+    }
+
     @Override
     public String introduce() {
-        return isIn(klass) ?
-                String.format("My name is %s. I am %d years old. I am a student. I am in class %d.", name, age, klass.getNumber())
-                :
-                String.format("My name is %s. I am %d years old. I am a student.",name,age);
+        if (isLeader) {
+            return String.format("My name is %s. I am %d years old. I am a student. I am the leader of class %d.", name, age, klass.getNumber());
+        } else {
+            return isIn(klass) ?
+                    String.format("My name is %s. I am %d years old. I am a student. I am in class %d.", name, age, klass.getNumber()) :
+                    String.format("My name is %s. I am %d years old. I am a student.", name, age);
+        }
     }
 }
